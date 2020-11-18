381de1be669f1a4392185d53a4e22ed22c8bc63b
racross1 <racross1@gmail.com>
2020-11-18 10:35:13 -0800

    v1 of Self Person Lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
 # your code goes here
+require 'pry'
+class Person
+attr_reader :name, :happiness, :hygiene 
+attr_accessor :bank_account
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end 
+
+    def set_happiness=(score)
+        @happiness = score
+        
+        # if @happiness < 11
+        # @happiness = happiness
+        # else 
+        #     puts "out of range"
+        # end 
+    end  
+        #@happiness.to_a(1..10)
+end 
